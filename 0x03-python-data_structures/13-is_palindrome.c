4413e6c33e3e45c2c990f4ec3451482cda3c0f7f
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2024-03-16 15:26:21 +0300

    my commit

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -26,7 +26,6 @@ void reverse_listint(listint_t **head)
 /**
  * is_palindrome - checks if a linked list is a palindrome
  * @head: double pointer to the linked list
- *
  * Return: 1 if it is, 0 if not
  */
 int is_palindrome(listint_t **head)
